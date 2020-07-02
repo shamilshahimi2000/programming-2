9c04f5c1ca384503d6243026d58f21de4acd8852
shamilshahimi2000 <61263165+shamilshahimi2000@users.noreply.github.com>
2020-07-02 18:07:01 +0800

    Add files via upload

---
+++ assignment 3/info.java
@@ -0,0 +1,49 @@
+
+
+public class info extends Assignment3 {
+
+    private String GClubs;
+    private String Mof;
+    private String Function;
+    private String GLoft;
+    
+info(String GClubs, String Mof , String Function , String GLoft ){
+   this.GClubs = GClubs;
+   this.Mof = Mof;
+   this.Function = Function;
+   this.GLoft = GLoft;
+}
+
+    public String getGClubs() {
+        return GClubs;
+    }
+
+    public void setGClubs(String GClubs) {
+        this.GClubs = GClubs;
+    }
+
+    public String getMof() {
+        return Mof;
+    }
+
+    public void setMof(String Mof) {
+        this.Mof = Mof;
+    }
+
+    public String getFunction() {
+        return Function;
+    }
+
+    public void setFunction(String Function) {
+        this.Function = Function;
+    }
+
+    public String getGLoft() {
+        return GLoft;
+    }
+
+    public void setGLoft(String GLoft) {
+        this.GLoft = GLoft;
+    }
+
+}
